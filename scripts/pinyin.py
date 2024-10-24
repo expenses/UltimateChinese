a613c4bb9b9ece85b815c3aa6162c12d0a058410
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-10-25 01:32:43 +0800

    Fix all 4-char pinyin

--- scripts/pinyin.py
+++ scripts/pinyin.py
@@ -4,7 +4,7 @@ import sys
 col = Collection(sys.argv[1])
        
 for i, note in enumerate(col.get_note(id) for id in col.find_notes("")):
-    if "<" in note["Pinyin"] or "(" in note["Pinyin"] or "<" in note["Simplified"] or '"' in note["Simplified"] or note["Simplified"].startswith("A") or len(note["Simplified"]) > 3:
+    if "<" in note["Pinyin"] or "(" in note["Pinyin"] or "<" in note["Simplified"] or '"' in note["Simplified"] or note["Simplified"].startswith("A") or len(note["Simplified"]) > 5:
         continue
     fixed_simp = note["Simplified"].replace("\n", "").replace("&lt;br /&gt;B:", "\nB:")
     generated = gen_pinyin(fixed_simp, output_heteronyms=", " in note["Pinyin"])
