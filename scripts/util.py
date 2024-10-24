19e58810ba077f7751dff3d9f3b8b91ab9785454
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-10-24 23:43:18 +0800

    Standardize more pinyin

--- scripts/util.py
+++ scripts/util.py
@@ -2,9 +2,12 @@ import jieba
 from pypinyin import pinyin
 
 def gen_pinyin(hanzi_string):
+    output = ""
     if len(hanzi_string) == 1:
         heteronyms = pinyin(hanzi_string, heteronym=True)
         
-        return ' '.join(', '.join(''.join(heteronym) for heteronym in inner) for inner in heteronyms)
-    segmented = jieba.cut(hanzi_string)
-    return ' '.join(''.join((''.join(char) for char in pinyin(word))) for word in segmented)
+        output = ' '.join(', '.join(''.join(heteronym) for heteronym in inner) for inner in heteronyms)
+    else:
+        segmented = jieba.cut(hanzi_string)
+        output = ' '.join(''.join((''.join(char) for char in pinyin(word))) for word in segmented)
+    return output.replace("！", "!")
