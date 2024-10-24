404faf38874cd6a584eb6efc3effa85535507311
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-10-24 23:31:52 +0800

    Standardize hanzi that use numbers for tones

--- scripts/pinyin.py
+++ scripts/pinyin.py
@@ -4,7 +4,7 @@ import sys
 col = Collection(sys.argv[1])
        
 for i, note in enumerate(col.get_note(id) for id in col.find_notes("")):
-    if note["Pinyin"].startswith("<") or len(note["Simplified"]) > 1:# or not note["Pinyin"].contains(set("1", "2", "3", "4")):
+    if note["Pinyin"].startswith("<") or len(note["Simplified"]) > 1  or not any(str(num) in note["Pinyin"] for num in range(1, 5)):
         continue
     if note["Pinyin"] != gen_pinyin(note["Simplified"]):
         print(f"{note["Simplified"]}, '{note["Pinyin"]}', '{gen_pinyin(note["Simplified"])}', {i}")
