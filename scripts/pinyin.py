78521eccff512c3b7a409bd2c6ec40a0fd48ca84
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-10-25 00:56:08 +0800

    Add more missing pinyin

--- scripts/pinyin.py
+++ scripts/pinyin.py
@@ -4,10 +4,16 @@ import sys
 col = Collection(sys.argv[1])
        
 for i, note in enumerate(col.get_note(id) for id in col.find_notes("")):
-    if "<" in note["Pinyin"] or "(" in note["Pinyin"] or note["Pinyin"] != "" or "<" in note["Simplified"] or len(note["Simplified"]) != 1:
+    if "<" in note["Pinyin"] or "(" in note["Pinyin"] or note["Pinyin"] != "" or "<" in note["Simplified"] or '"' in note["Simplified"] or note["Simplified"].startswith("A"):
         continue
-    if note["Pinyin"] != gen_pinyin(note["Simplified"]):
-        print(f"{note["Simplified"]}, '{note["Pinyin"]}', '{gen_pinyin(note["Simplified"])}', {i}")
-        note["Pinyin"] = gen_pinyin(note["Simplified"])
+    fixed_simp = note["Simplified"].replace("&lt;br /&gt;B:", "\nB:")
+    generated = gen_pinyin(fixed_simp)
+    if note["Pinyin"] != generated:
+        print(f"{note["Simplified"]}, '{note["Pinyin"]}', '{generated}', {i}")
+        note["Pinyin"] = generated
+        #col.update_note(note)
+    elif note["Simplified"] != fixed_simp:
+        print(f"{note["Simplified"]}, '{fixed_simp}', {i}")
+        note["Simplified"] = fixed_simp
         #col.update_note(note)
         
