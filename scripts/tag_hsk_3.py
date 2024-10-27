983f4eaa6ac842be73988c5ae8fc55939db603da
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-10-27 16:03:58 +0800

    Fix tag_hsk_3 script

--- scripts/tag_hsk_3.py
+++ scripts/tag_hsk_3.py
@@ -1,11 +1,11 @@
 # coding: utf-8
-from scripts import hsk_3_mappings
+from hsk_3_mappings import gen_mappings
 import sys
 from anki.collection import Collection
 
 col = Collection(sys.argv[1])
 
-mappings = hsk_3_mappings.gen_mappings()
+mappings = gen_mappings()
 for i, note in enumerate(col.get_note(id) for id in col.find_notes("")):
     if note["Simplified"] in mappings:
         tag = f"UltimateChinese::HSK3.0::{mappings[note["Simplified"]]}"
